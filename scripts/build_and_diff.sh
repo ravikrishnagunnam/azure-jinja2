f9d5c7ab64c0ba98654c70852d5356862131808b
sebbrochet <contact@sebbrochet.com>
2016-03-23 09:06:44 -0400

    Changed JSON pretty-printing to keep ordering (requires jq)

--- scripts/build_and_diff.sh
+++ scripts/build_and_diff.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-j2 azuredeploy.json.j2 | python -m json.tool > generated/azuredeploy.json
+j2 azuredeploy.json.j2 | jq . > generated/azuredeploy.json
+cat generated/azuredeploy.json | python -m json.tool > generated/azuredeploy.formatted.json
 cat reference/azuredeploy.original.json | python -m json.tool > reference/azuredeploy.formatted.json
-diff reference/azuredeploy.formatted.json generated/azuredeploy.json
+diff reference/azuredeploy.formatted.json generated/azuredeploy.formatted.json
